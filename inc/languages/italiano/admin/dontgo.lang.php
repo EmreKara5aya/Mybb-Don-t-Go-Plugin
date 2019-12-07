2cb62e8f589fa54bd205ac48a9567d1bce879189
Emre Karakaya <emrekarakaya@yandex.ru>
2019-12-07 18:16:55 +0300

    Aggiunti file in lingua italiana.
    İtalyanca dil dosyaları eklendi.

---
+++ inc/languages/italiano/admin/dontgo.lang.php
@@ -0,0 +1,18 @@
+<?php
+/* 
+			@Emre Karakaya
+			@www.emrekarakaya.com.tr
+*/
+
+$l['eklentiad'] = "Mybb Don't Go Plugin";
+$l['eklentiac'] = "Integrato Don't Go.js in Mybb";
+$l['aygrtitle'] = "Mybb Don\'t Go Impostazioni";
+$l['aygrdes'] = "Puoi Impostazioni Mybb Don\'t Go";
+$l['dontgomesajtit'] = "Inserisci nuovi titoli.";
+$l['dontgomesajdes'] = "Dopo un certo periodo di tempo, il titolo cambierà automaticamente e apparirà il titolo inserito.";
+$l['dontgomesaj'] = "Don't Go :(.";
+$l['dontgofavtit'] = "Inserisci il nuovo Favicon";
+$l['dontgofavdes'] = "Dopo un certo periodo di tempo cambieranno anche il titolo e la favicon. Specificare solo il percorso dell'immagine per la favicon predefinita. Immagini di esempio / favicon.ico Nel codice apparirà come site/images/favicon.ico.";
+$l['dontgozatit'] = "Specifica l'ora";
+$l['dontgozades'] = "Il titolo e la favicon cambiano automaticamente dopo il tempo inserito di seguito. Inserisci la durata in secondi.";
+?>
